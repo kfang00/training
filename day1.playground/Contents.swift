fd0435130489b91c2117dc9fca35593f2a1437e1
Kayla Fang <kayfang182@gmail.com>
2020-09-23 19:20:45 -0400

    completed day1

---
+++ day1.playground/Contents.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+// Variables
+//var str = "Hello, playground"
+//str = "Goodbye"
+
+//Strings and Integers
+//var str = "Hello, playground"
+var age = 38
+var population = 8_000_000
+
+//Multi-line strings
+var str1 = """
+This goes
+over multiple
+lines
+"""
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+//Doubles and Booleans
+var pi = 3.141
+var awesome = true
+
+//String interpolation
+var score = 85
+//var str = "Your score was \(score)"
+var results = "The test results are here: \(str)"
+
+//Constants - set once and never again
+let taylor = "swift"
+
+//Type annotations
+let str = "Hello, playground"
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
