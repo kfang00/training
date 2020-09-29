edce18d645307ad3fc6099b5febbe3938d8e40cb
Kayla Fang <kayfang182@gmail.com>
2020-09-29 02:19:16 -0400

    finished project 6

--- trainingDay3/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ trainingDay3/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -29,6 +29,9 @@ struct ContentView: View {
     @State private var showingScore = false
     @State private var scoreTitle = ""
     @State private var score = 0
+    
+    @State private var animationAmount = 0.0
+    @State private var isTapped = false
 
     var body: some View {
         ZStack {
@@ -50,10 +53,14 @@ struct ContentView: View {
                         self.flagTapped(number)
                     }) {
                         FlagImage(image: self.countries[number])
-                
                     }
+                    .opacity( number != self.correctAnswer && self.isTapped ? 0.25 : 1)
+                    .animation(.easeOut)
+                    .rotation3DEffect(.degrees(number == self.correctAnswer ? self.animationAmount : 0), axis: (x: 0, y: 1, z: 0))
+                    .colorMultiply (self.scoreTitle == "Wrong" ? .red: Color(red: 255, green: 255, blue: 255, opacity: 1))
+                    
                 }
-                
+            
                 Section {
                     Text("Current Score: \(score)")
                         .foregroundColor(.white)
@@ -73,6 +80,10 @@ struct ContentView: View {
         if number == correctAnswer {
             scoreTitle = "Correct"
             score += 1
+            withAnimation {
+                self.animationAmount += 360
+                self.isTapped = true
+            }
         } else {
             scoreTitle = "Wrong"
             score -= 1
@@ -85,6 +96,8 @@ struct ContentView: View {
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+        isTapped = false
+        scoreTitle = ""
     }
 }
 
--- trainingDay4/Animations/Animations/ContentView.swift
+++ trainingDay4/Animations/Animations/ContentView.swift
@@ -8,21 +8,92 @@
 
 import SwiftUI
 
+//Building custom transitions using ViewModifier
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content.rotationEffect(.degrees(amount), anchor: anchor)
+        .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(active: CornerRotateModifier(amount: -90, anchor: .topLeading), identity: CornerRotateModifier(amount: 0, anchor: .topLeading))
+        //call using .transition(.pivot)
+    }
+}
+
 struct ContentView: View {
     @State private var animationAmount =  0.0
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
+    @State private var letters = Array("Hello SwiftUI")
+    @State private var isShowingRed = false
 
     var body: some View {
-        Button("Tap Me") {
-            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
-                self.animationAmount += 360
+//Showing and hiding views with transitions
+        VStack {
+            Button("Tap Me") {
+                withAnimation {
+                    self.isShowingRed.toggle()
+                }
+            }
+            if isShowingRed {
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
             }
         }
-            .padding(50)
-            .background(Color.red)
-            .foregroundColor(.white)
-            .clipShape(Circle())
-            .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
-
+        
+//Animating gestures
+//        HStack(spacing: 0) {
+//            ForEach(0 ..< letters.count) {num in
+//                Text(String(self.letters[num]))
+//                .padding(5)
+//                    .font(.title)
+//                    .background(self.enabled ? Color.blue : Color.red)
+//                    .offset(self.dragAmount)
+//                    .animation(Animation.default.delay(Double(num) / 20))
+//            }
+//        }
+//    .gesture(
+//        DragGesture()
+//            .onChanged{self.dragAmount = $0.translation}
+//            .onEnded{_ in
+//                self.dragAmount = .zero
+//                self.enabled.toggle()
+//            }
+//    )
+        
+//        LinearGradient(gradient: Gradient(colors: [.yellow, .pink]), startPoint: .topLeading, endPoint: .bottomTrailing)
+//            .frame(width: 300, height: 200)
+//            .clipShape(RoundedRectangle(cornerRadius: 15))
+//            .offset(dragAmount)
+//        .gesture(
+//            DragGesture()
+//                .onChanged {self.dragAmount = $0.translation }
+//                .onEnded {_ in
+//                    withAnimation(.spring()) {
+//                        self.dragAmount = .zero
+//                    } //explict
+//                }
+//        )
+            //.animation(.spring()) //implict
+        
+//Controlling the animation stack
+//        Button("Tap Me") {
+//            self.enabled.toggle()
+//        }
+//        .frame(width: 200, height: 200)
+//        .background(enabled ? Color.blue : Color.red)
+//        .animation(.default)
+//        .foregroundColor(.white)
+//        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
+//        .animation(.interpolatingSpring(stiffness: 10, damping: 1))
     }
 }
 
