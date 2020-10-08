c242002d9e2b8294639a1226dc5fe2ef2d1c0820
Kayla Fang <kayfang182@gmail.com>
2020-10-08 18:28:13 -0400

    finished project 15

--- trainingDay7/Accessibility/Accessibility/ContentView.swift
+++ trainingDay7/Accessibility/Accessibility/ContentView.swift
@@ -31,6 +31,7 @@ struct ContentView: View {
     var body: some View {
         Stepper("Rate our service: \(rating)/5", value: $rating, in: 1...5)
             .accessibility(value: Text("\(rating) out of 5"))
+            .padding()
 
 //-------------
 //        Slider(value: $estimate, in: 0...50)
