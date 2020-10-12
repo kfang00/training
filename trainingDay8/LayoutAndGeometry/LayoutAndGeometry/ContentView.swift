32f00fc830bc73def315ba7f4c5fe5fa3421a6de
Kayla Fang <kayfang182@gmail.com>
2020-10-12 16:44:00 -0400

    finished project 18

--- trainingDay8/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ trainingDay8/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -8,6 +8,36 @@
 
 import SwiftUI
 
+struct OuterView: View {
+    var body: some View {
+        VStack {
+            Text("Top")
+            InnerView()
+                .background(Color.green)
+            Text("Bottom")
+        }
+    }
+}
+
+struct InnerView: View {
+    var body: some View {
+        HStack {
+            Text("Left")
+            GeometryReader { geo in
+                Text("Center")
+                    .background(Color.blue)
+                    .onTapGesture {
+                        print("Global center: \(geo.frame(in: .global).midX) x \(geo.frame(in: .global).midY)")
+                        print("Custom center: \(geo.frame(in: .named("Custom")).midX) x \(geo.frame(in: .named("Custom")).midY)")
+                        print("Local center: \(geo.frame(in: .local).midX) x \(geo.frame(in: .local).midY)")
+                    }
+            }
+            .background(Color.orange)
+            Text("Right")
+        }
+    }
+}
+
 extension VerticalAlignment {
     enum MidAccountAndName: AlignmentID {
         static func defaultValue(in d: ViewDimensions) -> CGFloat {
@@ -19,24 +49,84 @@ extension VerticalAlignment {
 }
 
 struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+    
     var body: some View {
-        HStack(alignment: .midAccountAndName) {
-            VStack {
-                Text("@twostraws")
-                .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
-                Image("paul-hudson")
-                Image("emoji")
-                    .resizable()
-                    .frame(width: 64, height: 64)
-            }
-
-            VStack {
-                Text("Full name:")
-                Text("PAUL HUDSON")
-                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
-                    .font(.largeTitle)
+        GeometryReader { fullView in
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(0..<50) { index in
+                        GeometryReader { geo in
+                            Rectangle()
+                                .fill(self.colors[index % 7])
+                                .frame(height: 150)
+                                .rotation3DEffect(.degrees(-Double(geo.frame(in: .global).midX - fullView.size.width / 2) / 10), axis: (x: 0, y: 1, z: 0))
+                        }
+                        .frame(width: 150)
+                    }
+                }
+                .padding(.horizontal, (fullView.size.width - 150) / 2)
             }
         }
+        .edgesIgnoringSafeArea(.all)
+//        GeometryReader { fullView in
+//            ScrollView(.vertical) {
+//                ForEach(0..<50) { index in
+//                    GeometryReader { geo in
+//                        Text("Row #\(index)")
+//                            .font(.title)
+//                            .frame(width: fullView.size.width)
+//                            .background(self.colors[index % 7])
+//                            .rotation3DEffect(.degrees(Double(geo.frame(in: .global).minY - fullView.size.height / 2) / 5), axis: (x: 0, y: 1, z: 0))
+//                    }
+//                    .frame(height: 40)
+//                }
+//            }
+//        }
+//        OuterView()
+//        .background(Color.red)
+//        .coordinateSpace(name: "Custom")
+//----------------
+//        VStack {
+//            GeometryReader { geo in
+//                Text("Hello, World!")
+//                    .frame(width: geo.size.width * 0.9, height: 40)
+//                    .background(Color.red)
+//            }
+//            .background(Color.yellow)
+//
+//            Text("More text")
+//                .background(Color.blue)
+//        }
+//        GeometryReader { geo in
+//            Text("Hello, World!")
+//                .frame(width: geo.size.width * 0.9)
+//                .background(Color.red)
+//        }
+//-----------
+//        Text("Hello, world!")
+//            //.position(x: 100, y: 100)
+//            .background(Color.red)
+//            .offset(x: 100, y: 100)
+//-------------
+//        HStack(alignment: .midAccountAndName) {
+//            VStack {
+//                Text("@twostraws")
+//                .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
+//                Image("paul-hudson")
+//                Image("emoji")
+//                    .resizable()
+//                    .frame(width: 64, height: 64)
+//            }
+//
+//            VStack {
+//                Text("Full name:")
+//                Text("PAUL HUDSON")
+//                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
+//                    .font(.largeTitle)
+//            }
+//        }
+//-------------
 //        VStack(alignment: .leading) {
 //            ForEach(0..<10) { position in
 //                Text("Number \(position)")
